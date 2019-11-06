e1640b2d63d5f3fe0304cc2885c0eaf7327cec03
Matthew Abraham <matthewabraham@localhost.localdomain>
2019-11-06 21:21:29 +0000

    mock api fetch

---
+++ src/mocks/WeatherData.js
@@ -0,0 +1,17 @@
+export const testData = ({
+    data: {
+        wind: 5.7,
+        clouds: 20,
+        humidity: 70,
+        locale: "Port of Spain, TT",
+        date: new Date().getUTCDate().toLocaleString()
+    }
+});
+
+export const successData = ({
+
+})
+
+export const errorData = ({
+    
+})
